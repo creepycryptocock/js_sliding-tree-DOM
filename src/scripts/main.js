ebbe764e6169cfa08452e031447779472b53a09f
Yurii <j.zherebilov15@gmail.com>
2025-06-17 00:40:26 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,28 @@
 'use strict';
 
-// write code here
+function listShowToggle() {
+  const tree = document.querySelector('.tree');
+
+  const liArray = tree.querySelectorAll('.tree li');
+
+  for (const li of liArray) {
+    const childUl = li.querySelector('ul');
+
+    if (childUl) {
+      const span = document.createElement('span');
+
+      const text = li.firstChild;
+
+      span.textContent = text.textContent;
+
+      li.insertBefore(span, text);
+      li.removeChild(text);
+
+      span.addEventListener('click', () => {
+        childUl.hidden = !childUl.hidden;
+      });
+    }
+  }
+}
+
+listShowToggle();
